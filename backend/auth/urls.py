f382d9e62d63a5701193f57586a1f58707e6f885
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-01-28 17:03:09 +0530

    Created API backend for authentication

---
+++ backend/auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+urlpatterns = [
+    path('/signup/',views.createUser,name='createUser'),
+    path('/login/',obtain_auth_token,name='login'),
+    path('/logout/',views.logout,name='logout')
+]
---
+++ backend/auth/views.py
@@ -0,0 +1,26 @@
+from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
+
+@api_view(['POST'])
+def createUser(request):
+    serializer=UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user=serializer.save()
+        token=Token.objects.create(user=user)
+        return Response({
+            'user':serializer.data,
+            'token':token.key
+        }, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response(status=status.HTTP_200_OK)
